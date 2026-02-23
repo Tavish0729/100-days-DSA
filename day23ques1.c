bd5eeaf5de507c49736bff6f9ea3723d16085aa5
Tavish0729 <tavishmahajan1234@gmail.com>
2026-02-23 21:16:36 +0530

    first commit

---
+++ day23ques1.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+    struct Node* result = NULL;
+    struct Node* tail = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        struct Node* temp;
+        if (l1->data <= l2->data) {
+            temp = l1;
+            l1 = l1->next;
+        } else {
+            temp = l2;
+            l2 = l2->next;
+        }
+
+        if (result == NULL) {
+            result = temp;
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    if (l2 != NULL)
+        tail->next = l2;
+
+    return result;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m, value;
+    struct Node* list1 = NULL;
+    struct Node* list2 = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        list1 = insertEnd(list1, value);
+    }
+
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &value);
+        list2 = insertEnd(list2, value);
+    }
+
+    struct Node* merged = mergeLists(list1, list2);
+    printList(merged);
+
+    return 0;
+}
