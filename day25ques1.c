ee3db2d4195cba37a05d9b77c0314add1ac95d19
Tavish0729 <tavishmahajan1234@gmail.com>
2026-02-25 22:31:45 +0530

    first commit

---
+++ day25ques1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n, key, count = 0;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = temp->next;
+        }
+    }
+
+    scanf("%d", &key);
+
+    temp = head;
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    printf("%d\n", count);
+
+    temp = head;
+    while (temp != NULL) {
+        struct Node* next = temp->next;
+        free(temp);
+        temp = next;
+    }
+
+    return 0;
+}
