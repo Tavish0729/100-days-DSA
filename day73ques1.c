c6a9b3811f2a4afe34efb9dd9df35917d167bb66
Tavish0729 <tavishmahajan1234@gmail.com>
2026-04-14 21:27:57 +0530

    first commit

---
+++ day73ques1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+
+    // Input string
+    scanf("%s", s);
+
+    int len = strlen(s);
+
+    // Count frequency of each character
+    for (int i = 0; i < len; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Find first non-repeating character
+    for (int i = 0; i < len; i++) {
+        if (freq[s[i] - 'a'] == 1) {
+            printf("%c\n", s[i]);
+            return 0;
+        }
+    }
+
+    // If no non-repeating character found
+    printf("$\n");
+
+    return 0;
+}
